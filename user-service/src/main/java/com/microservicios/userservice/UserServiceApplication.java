0f8e40da772a8d815beb1a07f014e3bb85a2a916
JOSE_ROSERO\jose2 <jxrosero1gmail.com>
2023-04-15 08:07:46 -0500

    eureka

--- user-service/src/main/java/com/microservicios/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/microservicios/userservice/UserServiceApplication.java
@@ -3,11 +3,13 @@ package com.microservicios.userservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
